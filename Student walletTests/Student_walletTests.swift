485f2e750be18fbaec17a068c33cb120f8daf9c8
aaetos724 <aaetos11@gmail.com>
2025-02-18 11:05:27 -0600

    Initial Commit

---
+++ Student walletTests/Student_walletTests.swift
@@ -0,0 +1,17 @@
+//
+//  Student_walletTests.swift
+//  Student walletTests
+//
+//  Created by Aaetos on 18/02/25.
+//
+
+import Testing
+@testable import Student_wallet
+
+struct Student_walletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
